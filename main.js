c0c68eae68943098c870b2b9e1a79a36c67f06b6
SHAKER <shakerdrais@gmail.com>
2019-08-04 10:48:33 +0300

    finished the assessment

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ function sum(x,y){
   return x+y
 }
 
+
 2) 
 function consoleReturn(x,y){
   console.log(x)
   return y
 }
+
 
 3) 
 var name="Alex"
@@ -35,17 +37,38 @@ var object2={
 }
 */
 
-//1) WRITE YOUR CODE UNDER THIS LINE         
+//1) WRITE YOUR CODE UNDER THIS LINE    
+sum = (x,y) => {
+  return x+y
+}     
 
-//2) WRITE YOUR CODE UNDER THIS LINE         
+//2) WRITE YOUR CODE UNDER THIS LINE 
+consoleReturn = (x,y) => {
+  console.log(x)
+  return y
+}        
 
-//3) WRITE YOUR CODE UNDER THIS LINE         
+//3) WRITE YOUR CODE UNDER THIS LINE   
+var name="Alex"
+var age=25
+var result="My name is: " + name + "and my age is: " + age      
 
-//4) WRITE YOUR CODE UNDER THIS LINE         
+//4) WRITE YOUR CODE UNDER THIS LINE   
+
+var food="Fried Chicken"
+var color="Blue"
+ object() ={
+  food:food,
+  color:color
+}     
 
 //5) WRITE YOUR CODE UNDER THIS LINE         
 
-
+object2 ()={
+  multi:func=(a,b) =>{
+    return a * b
+  }
+}
 
 
 
@@ -73,6 +96,23 @@ Output =>
 */
 
 // WRITE YOUR CODE UNDER THIS LINE
+// I didn't study OOP at all, I just remember that it has name and we can use constructor. 
+class Computer {
+  constructor(OS,RAM,CPU) {
+  let computer1 = OS,RAM,CPU
+  }
+
+// I didn't study OOP at all, I just remember that it has name and we can use constructor. 
+
+}
+doubleRam =>{
+  let computer1= {
+    OS: OS,
+    ram : RAM*2,
+    CPU: CPU,
+  }
+return  computer1
+}
 
 
 
@@ -86,6 +126,7 @@ please fix the errors inside them
 */
 
 // App Component
+import React, { Component } from 'react';
 import Tasks from './components/Tasks';
 
 export default class App extends Component {
@@ -93,14 +134,16 @@ export default class App extends Component {
     title: 'ELIZABETH GREENE',
     todos: ['eat', 'eat eat', 'eact again']
   };
-  changeTitle() {
+  changeTitle = ()=> {
     state.title = 'AGGREGOR ZOLDYCK'
   }
   render() {
     return (
-      <h1>App Component => state.title</h1>
+      <>
+      <h1>App Component = {this.state.title}</h1>
       <button onClick={this.changeTitle}>Change Title</button>
-      <Tasks tasks={this.todos} changeTitleFromChild={this.changeTitle} />
+      <Tasks tasks={this.Tasks} changeTitleFromChild={this.props.changeTitle} />
+      </>
     );
   }
 }
@@ -112,17 +155,18 @@ class Tasks extends Component {
   state = {
     day: "Sat"
   };
-  changeDay() {
+  changeDay = () => {
     day = 'Sun'
   }
-
   render() {
     return (
       <div>
-        <h1>Tasks Component => state.day</h1>
+        <h1>Tasks Component = {this.state.day}</h1>
         <button onClick={this.changeDay}>Change Tasks State</button>
-        <button onClick={changeTitle}>Change App State</button>
+        <button onClick={this.props.changeTitle}>Change App State</button>
       </div>
     );
   }
 }
+//IMPORTANT
+//line 166 am not sure of the answer but i know that we need to change on the parent from the child, so i think we should use props connected to function in the parent
